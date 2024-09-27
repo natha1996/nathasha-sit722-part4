a85e34f313a7cde9bc6b890f8d350fd33fb56ec0
natha1996 <73653061+natha1996@users.noreply.github.com>
2024-09-28 01:51:10 +1000

    Add files via upload

---
+++ example-3/variables.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "natha1week08"
+}
+
+variable location {
+  default = "australiasoutheast"
+}
+
+variable kubernetes_version { 
+  default = "1.30.3"
+}
