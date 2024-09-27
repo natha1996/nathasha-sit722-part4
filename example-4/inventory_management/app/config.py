d3fc7f85e1d82f8f38800f18dd10d8c7192a8170
natha1996 <73653061+natha1996@users.noreply.github.com>
2024-09-28 01:48:57 +1000

    Add files via upload

---
+++ example-4/inventory_management/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://week08_ow76_user:vwwUBNha0tNMZFtr29fKO0S5A22ThQ1b@dpg-crr1ki56l47c73cbid6g-a.oregon-postgres.render.com/week08_ow76')
